7924f467d325debac686322b418240ce4e139460
Carlos Alonso Martinez <carlos.alonso-martinez@outlook.com>
2017-05-18 17:36:18 +0200

    swagger afegit

--- api2/Controllers/ProducteController.cs
+++ api2/Controllers/ProducteController.cs
@@ -6,6 +6,9 @@ using api2.Models;
 
 namespace api2.Controllers
 {
+    /// <summary>
+    /// Acceso a la lista de cervezas
+    /// </summary>
     public class ProducteController : ApiController
     {
         static List<Producte> productes = new List<Producte>()
@@ -14,12 +17,20 @@ namespace api2.Controllers
             new Producte {Id=2,Nom="Guinness",Categoria="Stout",Preu=2.50 },
             new Producte {Id=3,Nom="Timmermans",Categoria="Lambic",Preu=3.75 }
         };
+        /// <summary>
+        /// Retorna todas las cervezas
+        /// </summary>
+        /// <returns></returns>
         // GET: api/Producte
         public IEnumerable<Producte> Get()
         {
             return productes;
         }
-
+        /// <summary>
+        /// Retorna cerveza seleccionada por id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET: api/Producte/5
         public IHttpActionResult Get(int id)
         {
@@ -31,6 +42,11 @@ namespace api2.Controllers
             
         }
 
+        /// <summary>
+        /// Agrega nueva cerveza en la lista
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         // POST: api/Producte
         public IHttpActionResult Post(Producte value)
         {
@@ -41,7 +57,12 @@ namespace api2.Controllers
             return Ok(value);
             //return CreatedAtRoute("DefaultApi", new {id = value.Id},value);
          }
-
+        /// <summary>
+        /// Modifica datos cerveza ya existente
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         // PUT: api/Producte/
         public IHttpActionResult Put(int id,[FromBody]Producte value)
         {
@@ -53,7 +74,11 @@ namespace api2.Controllers
             productes[index] = value;
             return Ok(value);
         }
-
+        /// <summary>
+        /// Elimina cerveza de la lista
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // DELETE: api/Producte/5
         public IHttpActionResult Delete(int id)
         {
---
+++ api2/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace api2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.
+            GlobalConfiguration.Configuration.EnableSwagger(c => c.SingleApiVersion("v1", "SomosTechies API")).EnableSwaggerUi();
+        }
+
+    }
+}
